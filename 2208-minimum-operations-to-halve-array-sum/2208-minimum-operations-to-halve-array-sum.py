d931b3675c30cfbc8187b23a256a4223e354a7cf
Tarun Sharma <tarunsha009@users.noreply.github.com>
2024-08-01 21:36:14 +0530

    Time: 812 ms (17.95%), Space: 31.3 MB (23.38%) - LeetHub

---
+++ 2208-minimum-operations-to-halve-array-sum/2208-minimum-operations-to-halve-array-sum.py
@@ -0,0 +1,17 @@
+import heapq
+class Solution:
+    def halveArray(self, nums: List[int]) -> int:
+        target = sum(nums) / 2
+        nums = [-num for num in nums]
+        heapq.heapify(nums)
+        ans = 0
+
+        while target > 0:
+            ans +=1
+            x = heapq.heappop(nums)
+            target += x/2
+            heapq.heappush(nums, x/2)
+
+        return ans
+
+        
